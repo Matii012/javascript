8ef1722c8e7225861e663bd89d17d4db7b0ea311
matias <matias_marrone01@hotmail.com>
2022-09-27 14:43:22 -0300

    realice cambios

--- scripts.js
+++ scripts.js
@@ -1,46 +1,56 @@
 let precio = 0;
 let iva = 1.21;
 
-while(precio === 0) {
 
-    let viaje = prompt("Ingrese el destino de su viaje entre ellos estan (Canada, España, Rusia, Francia)");
+const edad = prompt("ingrese su edad")
 
-    switch(viaje) {
+if (edad >= 18){
+    while(precio === 0) {
+
+        let viaje = prompt("Ingrese el destino de su viaje entre ellos estan (Canada, España, Rusia, Francia) (Recuerda que se le cobrara el IVA al viaje)");
+    
+    
+    
+        switch(viaje) {
+    
+            case "CANADA":
+            case "canada":
+            case "Canada":
+                precio = 350;
+                break;
+    
+            case "FRANCIA":
+            case "francia":
+            case "Francia":
+                precio = 500;
+                break;
+    
+            case "ESPAÑA":
+            case "españa":
+            case "España":
+                precio = 900;
+                break;
+    
+            case "RUSIA":
+            case "rusia":
+            case "Rusia":
+                precio = 700;
+                break;
+    
+            default:
+                alert("Ingresó un lugar que no se encuentra en la lista");
+                break;
+        }
         
-        case "CANADA":
-        case "canada":
-        case "Canada":
-            precio = 350;
-            break;
-
-        case "FRANCIA":
-        case "francia":
-        case "Francia":
-            precio = 500;
-            break;
-
-        case "ESPAÑA":
-        case "españa":
-        case "España":
-            precio = 900;
-            break;
-
-        case "RUSIA":
-        case "rusia":
-        case "Rusia":
-            precio = 700;
-            break;
-
-        default:
-            alert("Ingresó un lugar que no se encuentra en la lista");
-            break;
-
     }
+
     const resultado = parseInt(precio / iva);
 
 alert("El precio de su viaje es: $" + resultado);
+
+} else if (edad <=18) {
+    alert ("usted no tiene la edad suficiente para poder comprar un pasaje")
 }
 
-// const resultado = parseInt(precio / iva);
 
-// alert("El precio de su viaje es: $" + resultado);
+
